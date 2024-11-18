880c8e87caf084859f521bb4774356a4e9f3c2ab
branchwag <branchleannelmb@gmail.com>
2024-11-18 09:32:24 -0500

    gets webpage displaying on 8080

---
+++ README.md
@@ -0,0 +1,7 @@
+# Http Server
+
+Just getting a simple http server going in Java 
+
+
+mvn clean compile
+mvn exec:java -Dexec.mainClass="com.example.App"
--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,31 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.http.HttpStatus;
+import java.io.IOException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class App extends AbstractHandler {
+    public static void main( String[] args ) throws Exception {
+        Server server = new Server(8080);
+
+        server.setHandler(new App());
+
+        server.start();
+        server.join();
+    }
+
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpStatus.OK_200);
+
+        response.getWriter().println("<html><body>");
+        response.getWriter().println("<h1>Welcome</h1>");
+        response.getWriter().println("</body></html>");
+
+        baseRequest.setHandled(true);
     }
 }
